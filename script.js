4abfa534f31ed4cb431deca6789ac9d70c0083d6
“Nik <nikolaspreyes@gmail.com>
2025-05-16 15:13:53 -0700

    add all necessary elements

---
+++ script.js
@@ -0,0 +1,22 @@
+const myLibrary = [];
+
+// constructor to create book object
+const Book = function (bookObject) {};
+
+// uses the booMeta object and Book contructor to create book object
+const bookObject = function (bookMeta) {};
+
+// Uses book object to create new card
+const bookCard = function (bookObject) {};
+
+// retrives manually entered info from form (author and title required)
+const bookMeta = function () {};
+
+// stores new book object inside the library array
+const storeBook = function () {};
+
+const generateID = function () {};
+
+//  Then, add a separate function to the script (not inside the constructor) that
+//  can take some arguments (bookMeta), create a book from those arguments (bookObject), and store the new book
+//  object into an array (storeBook).
