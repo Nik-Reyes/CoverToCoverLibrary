9b7ccf34d8f927d9a1ef08ac27f4b3435e15bf82
“Nik <nikolaspreyes@gmail.com>
2025-05-16 18:35:46 -0700

    feature: add basic logic to create a new book object

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 const myLibrary = [];
+const inputButton = document.querySelector('.add-book');
+
+inputButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  const bookInfo = getInputControls(e);
+  bookObject(bookInfo);
+});
+
+// let JS look for input elements instead of manual search/query
+const getInputControls = function (e) {
+  const formElements = Array.from(e.target.form.elements);
+  return formElements
+    .filter((element) => element instanceof HTMLInputElement)
+    .map((input) => input.value);
+};
 
 // constructor to create book object
-const Book = function (bookObject) {};
+const Book = function (title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+};
 
-// uses the booMeta object and Book contructor to create book object
-const bookObject = function (bookMeta) {};
-
-// Uses book object to create new card
-const bookCard = function (bookObject) {};
-
-// retrives manually entered info from form (author and title required)
-const bookMeta = function () {};
-
-// stores new book object inside the library array
-const storeBook = function () {};
-
-const generateID = function () {};
+// uses inputs and Book contructor to create book object
+const bookObject = function (bookInfo) {
+  myLibrary.push(new Book(bookInfo[0], bookInfo[1], bookInfo[2]));
+  console.log(myLibrary);
+};
 
 //  Then, add a separate function to the script (not inside the constructor) that
-//  can take some arguments (bookMeta), create a book from those arguments (bookObject), and store the new book
+//  can take some arguments , create a book from those arguments (bookObject), and store the new book
 //  object into an array (storeBook).
