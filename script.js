c12b96a80c8e860328ed3a681c7d508262581d57
“Nik <nikolaspreyes@gmail.com>
2025-05-17 17:32:06 -0700

    refactor: use FormData object and clear form after submission

--- script.js
+++ script.js
@@ -2,32 +2,33 @@ const myLibrary = [];
 const inputButton = document.querySelector('.add-book');
 
 inputButton.addEventListener('click', (e) => {
-  e.preventDefault();
-  const bookInfo = getInputControls(e);
-  bookObject(bookInfo);
+  const bookMeta = getBookMetaObject(e);
+  const newBook = createNewbook(bookMeta);
 });
 
 // let JS look for input elements instead of manual search/query
-const getInputControls = function (e) {
-  const formElements = Array.from(e.target.form.elements);
-  return formElements
-    .filter((element) => element instanceof HTMLInputElement)
-    .map((input) => input.value);
+const getBookMetaObject = function (e) {
+  const form = e.target.form;
+  // FormData returns an iterable object of key value pairs (name attribute of input is the key, the input value is the value)
+  const formData = new FormData(form);
+
+  form.reset();
+  // Object.fromEntries transforms the iterable into an object of key value pairs
+  return Object.fromEntries(formData);
 };
 
 // constructor to create book object
-const Book = function (title, author, pages) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
+const Book = function (bookMeta) {
+  this.title = bookMeta.title;
+  this.author = bookMeta.author;
+  this.pages = bookMeta.pages;
+  this.ID = crypto.randomUUID();
 };
 
 // uses inputs and Book contructor to create book object
-const bookObject = function (bookInfo) {
-  myLibrary.push(new Book(bookInfo[0], bookInfo[1], bookInfo[2]));
+const createNewbook = function (bookMeta) {
+  const newBook = new Book(bookMeta);
+  myLibrary.push(newBook);
   console.log(myLibrary);
+  return newBook;
 };
-
-//  Then, add a separate function to the script (not inside the constructor) that
-//  can take some arguments , create a book from those arguments (bookObject), and store the new book
-//  object into an array (storeBook).
